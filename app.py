76aed245829a636dee3c9a0912f16681cb3b6ae6
Maxtorion <mateuszszczepanski00@gmail.com>
2020-10-26 20:34:47 +0100

    Tested data retrieval from target API

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
-
+from flask import render_template
+import requests
+import json
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    request = requests.get(
+        'https://www.googleapis.com/books/v1/volumes?q=Hobbit'
+    )
+    return render_template('books.html', books=json.loads(request.text)['items'])
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
